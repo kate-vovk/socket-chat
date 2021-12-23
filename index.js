25b98c43b4371000bf7239a09f845b7b1dbd6b36
Kateryna Vovk <vovk-k@bubuntu-lx.nixsolutions.com>
2021-12-23 16:52:48 +0200

    add room

--- index.js
+++ index.js
@@ -15,20 +15,20 @@ io.on('connection', (socket) => {
 
     console.log('a user ' + user.userName + ' connected');
   
-    socket.on('join', ({userId, userName}) => {
+    socket.on('join', ({userId, userName, roomname}) => {
       console.log('user ' + userName + ' joined');
-      socket.broadcast.emit('joined', {userId, userName, type: 'info', message: 'joined chat'});
+      socket.broadcast.to(roomname).emit('joined', {userId, userName, type: 'info', message: 'joined chat'});
+      socket.join(roomname);
     });
 
-    socket.on('leave', ({userId, userName}) => {
+    socket.on('leave', ({userId, userName, roomname}) => {
       console.log('user ' + userName + ' left chat');
-      socket.broadcast.emit('left', {userId, userName, type: 'info', message: 'left chat'});
+      socket.broadcast.to(roomname).emit('left', {userId, userName, type: 'info', message: 'left chat'});
     });
 
-    socket.on('my-message', ({message, userId, userName}) => {
+    socket.on('my-message', ({message, userId, userName, roomname}) => {
       console.log(userName + ' wrote message: ' + message);
-      // socket.emit('chat-broadcast', {userId, userName, type: 'message', message});
-      io.emit('chat-broadcast', {userId, userName, type: 'message', message});
+      io.to(roomname).emit('chat-broadcast', {userId, userName, type: 'message', message});
 
     });
 
